869739d1e220166e31d38ee0d9065f79b9eef85b
Shawn James <shawnjames.dev@icloud.com>
2023-02-19 19:53:28 -0700

    Setup storyboard

--- TTI-Coding-Challenge/TTI-Coding-Challenge/Supporting Files/AppDelegate.swift
+++ TTI-Coding-Challenge/TTI-Coding-Challenge/Supporting Files/AppDelegate.swift
@@ -7,8 +7,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -28,6 +26,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
